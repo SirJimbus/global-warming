c03fd27510f92d0fc3e4c5d78b0af41e9347ee73
FedeB <108481507+SirJimbus@users.noreply.github.com>
2023-02-20 18:48:59 +0100

    reroute

---
+++ src/App.js
@@ -0,0 +1,69 @@
+import React, { useState, useEffect } from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Temperature from "./pages/temperature/Temperature";
+import Co2 from "./pages/co2/Co2";
+import Methane from "./pages/methane/Methane";
+import No2 from "./pages/no2/No2";
+import Polar from "./pages/polar/Polar";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <div>
+        <Temperature />
+      </div>
+    ),
+  },
+  {
+    path: "/co2",
+    element: (
+      <div>
+        <Co2 />
+      </div>
+    ),
+  },
+  {
+    path: "/methane",
+    element: (
+      <div>
+        <Methane />
+      </div>
+    ),
+  },
+  {
+    path: "/no2",
+    element: (
+      <div>
+        <No2 />
+      </div>
+    ),
+  },
+  {
+    path: "/polar",
+    element: (
+      <div>
+        <Polar />
+      </div>
+    ),
+  },
+]);
+
+function App() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 1500); // simulazione di caricamento dati
+  }, []);
+
+  return (
+    <>
+      {isLoading && <p>Caricamento...</p>}
+      {!isLoading && <RouterProvider router={router} />}
+    </>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,55 +1,6 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Temperature from "./pages/temperature/Temperature";
-import Co2 from "./pages/co2/Co2";
-import Methane from "./pages/methane/Methane";
-import No2 from "./pages/no2/No2";
-import Polar from "./pages/polar/Polar";
+import ReactDOM from "react-dom";
+import App from "./App";
 import "./style.css";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: (
-      <div>
-        <Temperature />
-      </div>
-    ),
-  },
-  {
-    path: "/co2",
-    element: (
-      <div>
-        <Co2 />
-      </div>
-    ),
-  },
-  {
-    path: "/methane",
-    element: (
-      <div>
-        <Methane />
-      </div>
-    ),
-  },
-  {
-    path: "/no2",
-    element: (
-      <div>
-        <No2 />
-      </div>
-    ),
-  },
-  {
-    path: "/polar",
-    element: (
-      <div>
-        <Polar />
-      </div>
-    ),
-  },
-]);
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<RouterProvider router={router} />);
+ReactDOM.render(<App />, document.getElementById("root"));
